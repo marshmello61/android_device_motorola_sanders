6db2703e42e35877c8ae8dc03db38343b7130668
marshmello61 <ultramayur123@gmail.com>
2019-09-10 11:01:15 +0000

    nuke LineageActions
    Signed-off-by: marshmello61 <ultramayur123@gmail.com>

--- device.mk
+++ device.mk
@@ -129,8 +129,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/cacert_location.pem:system/vendor/etc/cacert_location.pem
 
 # LineageActions
-PRODUCT_PACKAGES += \
-    LineageActions
+#PRODUCT_PACKAGES += \
+#    LineageActions
 
 #Lineage HALs
 # Trust
